403a09e6659d624837f2729599cb70f7808feb86
Sergey Kievskiy <kievsan@mail.ru>
2022-02-16 10:24:04 +0300

    Edit java files

--- Person.java
+++ Person.java
@@ -1,10 +1,12 @@
 // package knhel7.jd14.jpat4.task11;
 
+import java.util.OptionalInt;
+
 public class Person {
 
     protected final String name;
     protected final String surname;
-    protected int age;
+    protected OptionalInt age;
     protected String city;
 
     public Person(String name, String surname) {
@@ -13,22 +15,25 @@ public class Person {
     }
 
     public boolean hasAge() {
-        return age > 0;
+        return age.isPresent();
     }
     public boolean hasAddress() {
         return !city.isEmpty();
     }
-    public int happyBirthday() {
-        return hasAge() ? age++ : age;
-    }
-    public String setAddress(String address) {
-        city = address;
-        return city;
+
+    public Person happyBirthday() {
+        if (hasAge()) {
+            int intAge = getIntAge();
+            this.age = OptionalInt.of(++intAge);
+        }
+        return this;
     }
 
     public String getName() { return name; }
     public String getSurname() { return surname; }
-    public int getAge() { return age; }
+    public OptionalInt getAge() { return age; }
+    public int getIntAge() { return  age.orElse(0); }
+    public String getStrAge() { return  hasAge() ? ", " + getIntAge() + "y." : ""; }
     public String getAddress() { return city; }
 
     public PersonBuilder newFamilyMemberBuilder() {
@@ -38,7 +43,7 @@ public class Person {
     @Override
     public String toString() {
         return surname + " " + name +
-                (hasAge() ? ", " + age + "y." : "") +
+                getStrAge() +
                 (hasAddress() ? ", " + city : "");
     }
 
--- PersonBuilder.java
+++ PersonBuilder.java
@@ -1,11 +1,15 @@
 // package knhel7.jd14.jpat4.task11;
 
+import java.util.OptionalInt;
+
 public class PersonBuilder implements PersonBuilderable {
 
     protected String name;
     protected String surname;
-    protected int age;
+    protected OptionalInt age;
     protected String city;
+
+    PersonBuilder() { initialBuilder(); }
 
     @Override
     public PersonBuilder setName(String name) {
@@ -21,26 +25,37 @@ public class PersonBuilder implements PersonBuilderable {
 
     @Override
     public PersonBuilder setAge(int age) {
-        this.age = age;
+        this.age = OptionalInt.of(age);
         return this;
     }
 
     @Override
     public PersonBuilder setAddress(String address) {
         this.city = address;
-        return  this;
+        return this;
     }
 
     @Override
-    public Person build()  throws IllegalArgumentException, IllegalStateException {
+    public Person build() throws IllegalArgumentException, IllegalStateException {
         if (name.isEmpty() || surname.isEmpty())
             throw new IllegalStateException("The necessary data is missing!");
         Person person = new Person(name, surname);
-        if (age < 0)
+        if (age.orElse(0) < 0)
             throw new IllegalArgumentException("Unacceptable age!");
-        person.age = this.age;
+        person.age = age;
         person.city = this.city;
+
+        initialBuilder();
+        
         return person;
     }
 
+    private void initialBuilder() {
+        this.name = "";
+        this.surname = "";
+        this.age = OptionalInt.empty();
+        this.city = "";
+    }
+
 }
+
